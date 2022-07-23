7f4cf27826b142eddbf6ca96dde6181e538b5d13
liaisontw <liaison.tw@gmail.com>
2022-07-23 22:33:27 +0800

    unitcafe step1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+      <h2>statistics</h2>
+      <h3>good {good}</h3>
+      <h3>neutral {neutral}</h3>
+      <h3>bad {bad}</h3>
+    </div>
+  )
+}
+
+export default App

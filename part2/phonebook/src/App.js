0c4df232aa72e814bc8c40dcb3f47fe1583a8454
liaisontw <liaison.tw@gmail.com>
2022-08-04 11:09:01 +0800

    2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter     from './components/Filter'
 import Persons    from './components/Persons'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',      number: '040-123456',    id: 1 },
-    { name: 'Ada Lovelace',     number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov',      number: '12-43-234345',  id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [ persons,   setPersons   ] = useState([]);
   const [ newName,   setNewName   ] = useState( '' );
   const [ newNumber, setNewNumber ] = useState( '' );
   const [ newSearch, setNewSearch ] = useState( '' );
   const [ newFilter, setNewFilter ] = useState( persons );
+
+  useEffect(() => {    
+    axios      
+      .get('http://localhost:3001/persons')      
+      .then(response => {        
+        setPersons(response.data)
+        setNewFilter(response.data)      
+      })  
+  }, [])  
 
   const handleNewFilter = (event) => {
       const searchTarget = event.target.value;
@@ -50,6 +55,7 @@ const App = () => {
   const handleNumberKeyin = (event) => {
       setNewNumber(event.target.value)
   }
+
   return (
     <div>
       <h2>Phonebook</h2>

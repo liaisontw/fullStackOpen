6734eccc171c91f7c64f349b3df716f44eb0cf08
liaisontw <liaison.tw@gmail.com>
2022-08-06 00:07:38 +0800

    2.12* Data for countries, step1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,10 +1,12 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+
 const App = () => {
   const [ countries, setCountries ] = useState([]);
-  const [ query,     setQuery     ] = useState('')
-  const [ list,      setList      ] = useState([])
+  const [ query,     setQuery     ] = useState('');
+  const [ list,      setList      ] = useState([]);
+  const [ country,   setCountry   ] = useState('');
 
   useEffect(() => {    
     axios      
@@ -20,19 +22,61 @@ const App = () => {
     const filterCountries = countries.filter(
       (country) => country.name.common.toLowerCase().search( queryValue.toLowerCase() ) !== -1
     );
+ 
     setList(filterCountries);
-}
+    if ( 1 === filterCountries.length ) {
+      setCountry(filterCountries[0]);
+    }
+  }
+
+
+  const showCountries = () => {
+    const count = list.length;
+    if ( 10 < count ) {
+      return(
+        <p>Too many matches, specify another filter</p>
+      )
+    }
+ 
+    if ( 1 === count ) {
+      const name = country.name.common;
+      const capital = country.capital[0];
+      const area = country.area;
+      const languages = country.languages;
+      const flag = country.flags.png;
+      const langList = Object.values(languages).map(val => val);
+      
+      return (
+        <div>
+          <h1>{name}</h1>
+          <p>capital {capital}</p>
+          <p>area {area}</p>
+          <p>languages: </p>  
+          <ul>
+            { langList.map( val => 
+              <li key={val}>{val}</li>
+            )}
+          </ul>
+          <img src={flag} alt={name} width="100px" />
+        </div>
+      )  
+    }
+
+    return (
+      <div>
+          {list.map(country =>
+            <p key={country.name.common}>{country.name.common}</p>
+          )}
+      </div>
+    )
+  }
 
   return (
     <div>
       <div>
           find countries <input onChange={handleQuery} value={query}></input>
       </div>
-      <div>
-          {list.map(country =>
-            <p key={country.name.common}>{country.name.common}</p>
-          )}
-      </div>
+      {showCountries()}
     </div>
   )
 }

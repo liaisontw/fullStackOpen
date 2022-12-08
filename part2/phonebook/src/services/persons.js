f83ddadd6beb7c3d466617a306845137a0cdeaeb
liaisontw <liaison.tw@gmail.com>
2022-12-08 11:30:11 +0800

    modify baseUrl

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 import axios from 'axios';
-const baseUrl = '/api/persons';
+//const baseUrl = '/api/persons';
+const baseUrl = 'http://localhost:3001/persons';
 
 const getAll = () => {
     const  request = axios.get(baseUrl);
